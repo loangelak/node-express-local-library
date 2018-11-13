bfba712a0d0e2240ed160ce32ece92c19b770a20
loangelak <loangela@optonline.net>
2018-11-12 21:10:29 -0500

    Add files via upload

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -19,17 +20,43 @@ AuthorSchema
 });
 
 // Virtual for author's lifespan
-AuthorSchema
-.virtual('lifespan')
-.get(function () {
-  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
-});
+// AuthorSchema
+// .virtual('lifespan')
+// .get(function () {
+//   return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+// });
 
 // Virtual for author's URL
 AuthorSchema
 .virtual('url')
 .get(function () {
-  return '/catalog/author/' + this._id;
+  return '/catalog/author/'+this._id
+});
+
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  var lifetime_string='';
+  if (this.date_of_birth) {
+      lifetime_string=moment(this.date_of_birth).format('MMMM Do, YYYY');
+      }
+  lifetime_string+=' - ';
+  if (this.date_of_death) {
+      lifetime_string+=moment(this.date_of_death).format('MMMM Do, YYYY');
+      }
+  return lifetime_string
+});
+
+AuthorSchema
+.virtual('date_of_birth_yyyy_mm_dd')
+.get(function () {
+  return moment(this.date_of_birth).format('YYYY-MM-DD');
+});
+
+AuthorSchema
+.virtual('date_of_death_yyyy_mm_dd')
+.get(function () {
+  return moment(this.date_of_death).format('YYYY-MM-DD');
 });
 
 //Export model
